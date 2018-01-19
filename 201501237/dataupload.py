5608d17d0c4fc5ebe0cd3ae6751b3a874929145f
niraj1997 <nirajvadhaiya@gmail.com>
2018-01-19 16:45:28 +0530

    Delete dataupload.py

--- 201501237/dataupload.py
+++
@@ -1,7 +0,0 @@
-import csv
-from elasticsearch import Elasticsearch, helpers
-
-es = Elasticsearch()
-with open('MOCK_DATA.csv','rU') as f:
-    reader = csv.DictReader(f)
-    helpers.bulk(es, reader, index='book_index', doc_type='bookmap')
